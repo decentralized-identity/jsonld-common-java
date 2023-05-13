9be642f417eed62b2bc0b4ebdef4f984edb77def
Markus Sabadello <markus@danubetech.com>
2023-05-13 17:09:52 +0200

    feat: Support dereferencing relative URIs.

--- src/main/java/foundation/identity/jsonld/JsonLDDereferencer.java
+++ src/main/java/foundation/identity/jsonld/JsonLDDereferencer.java
@@ -14,17 +14,34 @@ public class JsonLDDereferencer {
 
         private JsonLDObject jsonLdDocument;
         private URI baseUri;
+        private boolean allowRelativeWithoutBaseUri;
         private Predicate<JsonLDObject> predicate;
+
+        public Function(JsonLDObject jsonLdDocument, URI baseUri, boolean allowRelativeWithoutBaseUri, Predicate<JsonLDObject> predicate) {
+            this.jsonLdDocument = jsonLdDocument;
+            this.baseUri = baseUri;
+            this.allowRelativeWithoutBaseUri = allowRelativeWithoutBaseUri;
+            this.predicate = predicate;
+        }
+
+        public Function(JsonLDObject jsonLdDocument, URI baseUri, boolean allowRelativeWithoutBaseUri) {
+            this.jsonLdDocument = jsonLdDocument;
+            this.baseUri = baseUri;
+            this.allowRelativeWithoutBaseUri = allowRelativeWithoutBaseUri;
+            this.predicate = null;
+        }
 
         public Function(JsonLDObject jsonLdDocument, URI baseUri, Predicate<JsonLDObject> predicate) {
             this.jsonLdDocument = jsonLdDocument;
             this.baseUri = baseUri;
+            this.allowRelativeWithoutBaseUri = false;
             this.predicate = predicate;
         }
 
         public Function(JsonLDObject jsonLdDocument, URI baseUri) {
             this.jsonLdDocument = jsonLdDocument;
             this.baseUri = baseUri;
+            this.allowRelativeWithoutBaseUri = false;
             this.predicate = null;
         }
 
@@ -32,13 +49,14 @@ public class JsonLDDereferencer {
             this.jsonLdDocument = jsonLdDocument;
             this.baseUri = null;
             this.predicate = null;
+            this.allowRelativeWithoutBaseUri = false;
         }
 
         @Override
         public JsonLDObject apply(Object o) throws JsonLDDereferencingException {
 
             URI uri = null;
-            JsonLDObject result = null;
+            JsonLDObject result;
 
             if (o instanceof JsonLDObject) {
                 result = (JsonLDObject) o;
@@ -50,7 +68,7 @@ public class JsonLDDereferencer {
                 } catch (URISyntaxException ex) {
                     throw new JsonLDDereferencingException("Cannot dereference non-URI string: " + o);
                 }
-                result = findByIdInJsonLdObject(this.jsonLdDocument, uri, this.baseUri);
+                result = findByIdInJsonLdObject(this.jsonLdDocument, uri, this.baseUri, this.allowRelativeWithoutBaseUri);
             } else {
                 throw new JsonLDDereferencingException("Cannot dereference non-URI value: " + o);
             }
@@ -70,7 +88,9 @@ public class JsonLDDereferencer {
         }
     }
 
-    public static JsonLDObject findByIdInJsonLdObject(JsonLDObject jsonLdObject, URI uri, URI baseUri) {
+    public static JsonLDObject findByIdInJsonLdObject(JsonLDObject jsonLdObject, URI uri, URI baseUri, boolean allowRelativeWithoutBaseUri) {
+
+        if (baseUri == null && allowRelativeWithoutBaseUri) baseUri = URI.create("urn:uuid:dummy-base-uri");
 
         if (jsonLdObject.getId() != null) {
 
@@ -87,11 +107,11 @@ public class JsonLDDereferencer {
 
         for (Object value : jsonLdObject.getJsonObject().values()) {
             if (value instanceof Map) {
-                JsonLDObject foundJsonLDObject = findByIdInJsonLdObject(JsonLDObject.fromMap((Map<String, Object>) value), uri, baseUri);
+                JsonLDObject foundJsonLDObject = findByIdInJsonLdObject(JsonLDObject.fromMap((Map<String, Object>) value), uri, baseUri, allowRelativeWithoutBaseUri);
                 if (foundJsonLDObject != null) return foundJsonLDObject;
             }
             else if (value instanceof List) {
-                JsonLDObject foundJsonLDObject = findByIdInList((List<Object>) value, uri, baseUri);
+                JsonLDObject foundJsonLDObject = findByIdInList((List<Object>) value, uri, baseUri, allowRelativeWithoutBaseUri);
                 if (foundJsonLDObject != null) return foundJsonLDObject;
             }
         }
@@ -99,15 +119,19 @@ public class JsonLDDereferencer {
         return null;
     }
 
-    private static JsonLDObject findByIdInList(List<Object> list, URI uri, URI baseUri) {
+    public static JsonLDObject findByIdInJsonLdObject(JsonLDObject jsonLdObject, URI uri, URI baseUri) {
+        return findByIdInJsonLdObject(jsonLdObject, uri, baseUri, false);
+    }
+
+    private static JsonLDObject findByIdInList(List<Object> list, URI uri, URI baseUri, boolean allowRelativeWithoutBaseUri) {
 
         for (Object value : list) {
             if (value instanceof Map) {
-                JsonLDObject foundJsonLDObject = findByIdInJsonLdObject(JsonLDObject.fromMap((Map<String, Object>) value), uri, baseUri);
+                JsonLDObject foundJsonLDObject = findByIdInJsonLdObject(JsonLDObject.fromMap((Map<String, Object>) value), uri, baseUri, allowRelativeWithoutBaseUri);
                 if (foundJsonLDObject != null) return foundJsonLDObject;
             }
             else if (value instanceof List) {
-                JsonLDObject foundJsonLDObject = findByIdInList((List<Object>) value, uri, baseUri);
+                JsonLDObject foundJsonLDObject = findByIdInList((List<Object>) value, uri, baseUri, allowRelativeWithoutBaseUri);
                 if (foundJsonLDObject != null) return foundJsonLDObject;
             }
         }
